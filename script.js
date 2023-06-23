54b94e6b2d0e74cceaf5efa6ab112a97ba5107aa
siidny <39230882+siidny@users.noreply.github.com>
2023-06-23 22:48:26 +1000

    with owner
    delted old added new to remove Postcode

---
+++ script.js
@@ -0,0 +1,105 @@
+      const gsheetURL = "https://sheets.googleapis.com/v4/spreadsheets/1pdDoQnCOoMnjtEnSzRDzlzmz9gtBwdRY2Wh7GwcE7NA/values/Sheet1?key=AIzaSyAWleGQ7SUwXyt4DwnWlu1H8J1W8VI5pSA";
+
+        new Vue({
+            el: '#app',
+            data() {
+                return {
+                    searchInput: '',
+                    noResults: false,
+                    titles: [],
+                    filteredTitles: [],
+                    currentPage: 1,
+                    itemsPerPage: 10,
+                    initialLoad: true,
+                    titleProperties: [
+                        { key: 'classes', label: 'classes' },
+                        { key: 'schoolname', label: 'Language school name' },
+                        { key: 'location', label: 'Location' },
+                        { key: 'address', label: 'Address' },
+                        { key: 'suburb', label: 'Suburb' },
+                        { key: 'owner', label: 'Content owner' }
+                    ]
+                };
+            },
+            async mounted() {
+                try {
+                    const response = await axios.get(gsheetURL);
+                    this.parseData(response);
+                    this.initialLoad = false;
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            },
+            methods: {
+                filterTitles(event) {
+                    this.searchInput = event.target.value;
+                    this.currentPage = 1;
+                    const query = this.searchInput.toLowerCase();
+                    if (query.length < 1) {
+                        this.filteredTitles = [];
+                        this.initialLoad = true;
+                        return;
+                    }
+                   this.filteredTitles = this.titles.filter(title => {
+         const publicprivate = title.publicprivate ? title.publicprivate.trim().toLowerCase() : '';
+    const language = title.language ? title.language.trim().toLowerCase() : '';
+    const schoolname = title.schoolname ? title.schoolname.trim().toLowerCase() : '';
+    const location = title.location ? title.location.trim().toLowerCase() : '';
+    const address = title.address ? title.address.trim().toLowerCase() : '';
+    const suburb = title.suburb ? title.suburb.trim().toLowerCase() : '';
+    const state = title.state ? title.state.trim().toLowerCase() : '';
+    const owner = title.owner ? title.owner.trim().toLowerCase() : '';
+    const classes = title.classes ? title.classes.trim().toLowerCase() : '';
+
+    return publicprivate.includes(query) ||
+           language.includes(query) ||
+           schoolname.includes(query) ||
+           location.includes(query) ||
+           address.includes(query) ||
+           suburb.includes(query) ||
+           state.includes(query) ||
+           owner.includes(query) ||
+           classes.includes(query);
+                          
+                    ));
+                    this.noResults = this.filteredTitles.length === 0;
+
+                    this.initialLoad = false;
+                },
+                parseData(entries) {
+                    entries.data.values.slice(1).forEach(value => {
+                        const entry = {
+                            publicprivate: value[0],
+                              language: value[1],
+                            schoolname: value[2],
+                            location: value[3],
+                            address: value[4],
+                            suburb: value[5],
+                            state: value[6],
+                            owner: value[7],
+                            classes: value[8],
+                            unique: value[9]
+                        };
+                        this.titles.push(entry);
+                    });
+
+                    this.filteredTitles = this.titles;
+                    console.log(this.titles);
+                }
+            },
+            computed: {
+                paginatedTitles() {
+                    return this.titlesToDisplay.slice((this.currentPage - 1) * this.itemsPerPage, this.currentPage * this.itemsPerPage);
+                },
+                totalPages() {
+                    return Math.ceil(this.titlesToDisplay.length / this.itemsPerPage);
+                },
+                titlesToDisplay() {
+                    if (this.initialLoad) {
+                        return [];
+                    }
+                    return this.filteredTitles.length > 0 ? this.filteredTitles : this.titles;
+                }
+            }
+        })
+  
